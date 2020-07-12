9ec7966b14ff3dd83245a21b2a395c077128ade6
Jayesh Choubisa <26424936+jayeshchoubisa@users.noreply.github.com>
2020-07-12 21:35:22 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use('/api', apiRouter);
 
 // app.use('/', indexRouter);
